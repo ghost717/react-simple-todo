a6219cc19d0501281b98bbdf1bf8b37c0460c22b
jason7 <dawid.zawadzki7717@gmail.com>
2017-11-16 02:18:10 +0100

    added react-router / react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './components/Main';
+import About from './components/About';
+
+// Render the main component into the dom
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('app'));
+
+
+
+/*
+
+<Router history={browserHistory}>
+    <Route path="/" component={App} />
+    <Route path="/about" component={About} />
+</Router>
+
+import 'core-js/fn/object/assign';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './components/Main';
 
 // Render the main component into the dom
 ReactDOM.render(<App />, document.getElementById('app'));
+*/
