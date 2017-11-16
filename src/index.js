1855aebb5317a9efc1bef12994e765cf98382b67
jason7 <dawid.zawadzki7717@gmail.com>
2017-11-16 22:59:00 +0100

    rebuild app / react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,49 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
-import App from './components/Main';
+import Main from './components/Main';
 import About from './components/About';
+
+const Schedule = () => (
+  <div>
+    <ul>
+      <li>6/5 @ Evergreens</li>
+      <li>6/8 vs Kickers</li>
+      <li>6/14 @ United</li>
+    </ul>
+  </div>
+)
+
+const Content = () => (
+  <content>
+    <Switch>
+      <Route exact path='/' component={Main}/>
+      <Route path='/about' component={About}/>
+      <Route path='/schedule' component={Schedule}/>
+    </Switch>
+  </content>
+)
+
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>about</Link></li>
+        <li><Link to='/schedule'>Schedule</Link></li>
+      </ul>
+    </nav>
+  </header>
+)
+
+const App = () => (
+  <div>
+  	<Header />
+    <Content />
+  </div>
+)
 
 // Render the main component into the dom
 ReactDOM.render((
@@ -13,21 +51,3 @@ ReactDOM.render((
     <App />
   </BrowserRouter>
 ), document.getElementById('app'));
-
-
-
-/*
-
-<Router history={browserHistory}>
-    <Route path="/" component={App} />
-    <Route path="/about" component={About} />
-</Router>
-
-import 'core-js/fn/object/assign';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/Main';
-
-// Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
-*/
