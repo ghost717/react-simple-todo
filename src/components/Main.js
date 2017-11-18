049b1f82d0d3f6530ae32d074be7e5708066c9e3
jason7 <dawid.zawadzki7717@gmail.com>
2017-11-18 01:10:32 +0100

    rebuild Main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,63 +1,53 @@
 require('styles/App.css');
 import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
 
-const Title = ({todoCount}) => {
+import About from './About';
+import Todos from './Todos';
+
+const Content = () => {
+  return (
+    <main>
+      <Switch>
+        <Route exact path='/' component={Main}/>
+        <Route path="/todos" component={Todos}/>
+        <Route path='/about' component={About}/>
+      </Switch>
+    </main>
+  )
+}
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/todos'>Todos</Link></li>
+          <li><Link to='/about'>about</Link></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+const Main = () => {
   return (
     <div>
-       <div>
-          <h1>to-do ({todoCount})</h1>
-       </div>
-       <nav>
-
-       </nav>
+      <h1>Hello React!</h1>
     </div>
-  );
+  )
 }
 
-const TodoForm = ({addTodo}) => {
-  // Input Tracker
-  let input;
-  // Return JSX
+const Footer = () => {
   return (
-      <form onSubmit={(e) => {
-          e.preventDefault();
-          addTodo(input.value);
-          input.value = '';
-        }}>
-        <input className="form-control col-md-12" ref={node => {
-          input = node;
-        }} />
-        <br />
-      </form>
-  );
-};
-
-const Todo = ({todo, remove}) => {
-  // Each Todo
-  return (
-		  	<div className="list-item">
-		  		<h3>#{todo.id} {todo.name}</h3>
-		  		<p>{todo.content}</p>
-		  		<p>{todo.author}</p>
-		  		<p>{todo.dev}</p>
-		  		<p>{todo.createdAt}</p>
-		  		<a href="#" onClick={() => {remove(todo.id)}}>Usuń</a>
-		  	</div>
-  		);
+    <footer>
+      <a href="http://jwba.pl">jwba.pl - jason web apps</a>
+    </footer>
+  )
 }
 
-const TodoList = ({todos, remove}) => {
-  // Map through the todos
-  const todoNode = todos.map((todo) => {
-    return (<Todo todo={todo} key={todo.id} remove={remove}/>)
-  });
-  return (<div className="list" style={{marginTop:'30px'}}>{todoNode}</div>);
-}
-
-// Contaner Component
-// Todo Id
-window.id = 0;
-class TodoApp extends React.Component{
+class App extends React.Component{
   constructor(props){
     // Pass props to parent class
     super(props);
@@ -66,8 +56,6 @@ class TodoApp extends React.Component{
       data: []
     }
 
-    //https://5a088601519b7c0012caab20.mockapi.io/api/v1/react-simple-todo
-    //http://localhost:3000/api/users
     this.apiUrl = 'https://5a088601519b7c0012caab20.mockapi.io/api/v1/react-simple-todo'
   }
   // Lifecycle method
@@ -79,43 +67,17 @@ class TodoApp extends React.Component{
         this.setState({data:res.data});
       });
   }
-  // Add todo handler
-  addTodo(val){
-    // Assemble data
-    const todo = { name: val, content: 'lorem ipsum', author: 'admin', dev: 'dev1', createdAt: new Date() }
-    // Update data
-    axios.post(this.apiUrl, todo)
-       .then((res) => {
-          this.state.data.push(res.data);
-          this.setState({data: this.state.data});
-       });
-  }
-  // Handle remove
-  handleRemove(id){
-    // Filter all todos except the one to be removed
-    const remainder = this.state.data.filter((todo) => {
-      if(todo.id !== id) return todo;
-    });
-    // Update state with filter
-    axios.delete(this.apiUrl+'/'+id)
-      .then((res) => {
-        this.setState({data: remainder});      
-      })
-  }
  
   render(){
     // Render JSX
     return (
       <div>
-        <Title todoCount={this.state.data.length}/>
-        <TodoForm addTodo={this.addTodo.bind(this)}/>
-        <TodoList 
-          todos={this.state.data} 
-          remove={this.handleRemove.bind(this)}
-        />
+        <Header />
+        <Content />
+        <Footer />
       </div>
     );
   }
 }
 
-export default TodoApp;
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,49 +1,9 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import Main from './components/Main';
-import About from './components/About';
-
-const Schedule = () => (
-  <div>
-    <ul>
-      <li>6/5 @ Evergreens</li>
-      <li>6/8 vs Kickers</li>
-      <li>6/14 @ United</li>
-    </ul>
-  </div>
-)
-
-const Content = () => (
-  <content>
-    <Switch>
-      <Route exact path='/' component={Main}/>
-      <Route path='/about' component={About}/>
-      <Route path='/schedule' component={Schedule}/>
-    </Switch>
-  </content>
-)
-
-const Header = () => (
-  <header>
-    <nav>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/about'>about</Link></li>
-        <li><Link to='/schedule'>Schedule</Link></li>
-      </ul>
-    </nav>
-  </header>
-)
-
-const App = () => (
-  <div>
-  	<Header />
-    <Content />
-  </div>
-)
+import App from './components/Main';
 
 // Render the main component into the dom
 ReactDOM.render((
